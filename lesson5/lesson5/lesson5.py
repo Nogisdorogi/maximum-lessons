20d2ab84507f4b92de35b0f7e7dfc232960f34fd
Yuliya <you@example.com>
2019-06-12 13:02:01 +0300

    added lesson5

---
+++ lesson5/lesson5/lesson5.py
@@ -0,0 +1,23 @@
+person = {
+    "name":"Alex",
+    "age":16,
+    "gender":"female"
+    }
+print(person["age"])
+
+cars = ("bmw","reno","lada")
+
+cars2 = ["mersedes","kia"]
+
+cars2.append("honda")
+
+hours = 17
+if hours>=5 and hours<12:
+   print("morning")
+elif hours>12 and hours<=17:
+    print("day")
+elif hours >17 and hours<=21:
+    print("evening")
+elif hours >21 or hours <=5:
+    print("night")
+
